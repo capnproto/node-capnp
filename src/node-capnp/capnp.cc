82d1fe055091ae1558c2b9255507a645dee6fb6e
Jason Paryani <github@jparyani.com>
2015-02-19 13:47:31 -0800

    Add _id field for imported modules

--- src/node-capnp/capnp.cc
+++ src/node-capnp/capnp.cc
@@ -927,8 +927,13 @@ v8::Handle<v8::Value> setNative(const v8::Arguments& args) {
 v8::Local<v8::Object> schemaToObject(capnp::ParsedSchema schema, CapnpContext& context,
                                      v8::Handle<v8::Value> wrappedContext) {
   auto result = context.wrapper.wrap(new capnp::Schema(schema));
+  auto proto = schema.getProto();
 
-  for (auto nested: schema.getProto().getNestedNodes()) {
+  result->Set(v8::String::NewSymbol("_id"),
+              // 64-bit values must be stringified to avoid losing precision.
+              v8::String::New(kj::str(proto.getId()).cStr()));
+
+  for (auto nested: proto.getNestedNodes()) {
     kj::StringPtr name = nested.getName();
     result->Set(v8::String::NewSymbol(name.cStr()),
                 schemaToObject(schema.getNested(name), context, wrappedContext));
