e3ce43e2be7020abf708fa17aa734551b573f54c
Kenton Varda <kenton@sandstorm.io>
2016-03-27 05:47:13 -0700

    Include Javascript stack when converting to KJ exception.

--- src/node-capnp/capnp.cc
+++ src/node-capnp/capnp.cc
@@ -745,6 +745,7 @@ v8::Local<v8::Value> toJsException(kj::Exception&& exception) {
 
 kj::Exception fromJsException(v8::Handle<v8::Value> exception) {
   kj::Exception::Type type = kj::Exception::Type::FAILED;
+  kj::String description;
 
   if (exception->IsObject()) {
     v8::HandleScope scope(v8::Isolate::GetCurrent());
@@ -760,9 +761,16 @@ kj::Exception fromJsException(v8::Handle<v8::Value> exception) {
         type = kj::Exception::Type::UNIMPLEMENTED;
       }
     }
+
+    v8::Handle<v8::Value> stack = obj->Get(newSymbol("stack"));
+    if (!stack.IsEmpty() && !stack->IsUndefined()) {
+      description = toKjString(stack);
+    } else {
+      description = toKjString(exception);
+    }
   }
 
-  return kj::Exception(type, "(javascript)", 0, toKjString(exception));
+  return kj::Exception(type, "(javascript)", 0, kj::mv(description));
 }
 
 EmptyHandle throwTypeError(kj::StringPtr name, const std::type_info& type,
